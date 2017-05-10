8ed4171ad9d4266d45110d60498f8808fab3bfa7
Kieran Ramnarine <kieran.ramnarine@ryerson.ca>
2017-05-10 17:04:00 +0000

    modified:   node_helper.js

--- node_helper.js
+++ node_helper.js
@@ -18,7 +18,7 @@ module.exports = NodeHelper.create({
 	 * argument payload mixed - The payload of the notification.
 	 */
 	socketNotificationReceived: function(notification, payload) {
-		if (notification === "{{MODULE_NAME}}-NOTIFICATION_TEST") {
+		if (notification === "octomirror-module-NOTIFICATION_TEST") {
 			console.log("Working notification system. Notification:", notification, "payload: ", payload);
 			// Send notification
 			this.sendNotificationTest(this.anotherFunction()); //Is possible send objects :)
@@ -27,13 +27,13 @@ module.exports = NodeHelper.create({
 
 	// Example function send notification test
 	sendNotificationTest: function(payload) {
-		this.sendSocketNotification("{{MODULE_NAME}}-NOTIFICATION_TEST", payload);
+		this.sendSocketNotification("octomirror-module-NOTIFICATION_TEST", payload);
 	},
 
 	// this you can create extra routes for your module
 	extraRoutes: function() {
 		var self = this;
-		this.expressApp.get("/{{MODULE_NAME}}/extra_route", function(req, res) {
+		this.expressApp.get("/octomirror-module/extra_route", function(req, res) {
 			// call another function
 			values = self.anotherFunction();
 			res.send(values);
