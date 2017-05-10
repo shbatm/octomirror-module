8015f4688d89240bea03c7d5d8e04110189bbd6d
Kieran Ramnarine <kieran.ramnarine@ryerson.ca>
2017-05-10 18:20:29 -0400

    Update octomirror-module.js

--- octomirror-module.js
+++ octomirror-module.js
@@ -19,7 +19,7 @@ Module.register("octomirror-module", {
 		var wrapper = document.createElement("img");
 		wrapper.width = this.config.width;
 		wrapper.height = this.config.height;
-		wrapper.src = this.config.url;
+		wrapper.src = "192.168.1.19:8080/?action=stream";
 		return wrapper;
 	},
 	getHeader: function() {
