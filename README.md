c7d7d235f21ed90e4d682580e6e0166fc5249e80
shbatm <tim@shbatm.com>
2018-01-17 12:31:20 -0600

    Added live printer status updates

--- README.md
+++ README.md
@@ -1,13 +1,21 @@
-# Octomirror Module
-This is a module for a Magic Mirror device that will interact with an Octoprint Instance. 
+# Module: Display & Control OctoPrint Instance (Octomirror-module)
+
+This is a module for <a href="https://github.com/MichMich/MagicMirror">MagicMirror</a> that will interact with an Octoprint Instance. 
 
 The module will allow you to:
 - View a live feed of the raspicam stream
 - Start and stop prints
 - View G-Code viewer
 - Manually control the printer
+- Recieve live updates on printer status
 
 If you have any suggest, please let me know [by an issue](https://github.com/DongerZonie/octomirror-module/issues/new).
+
+## Requirements
+
+* Raspberry Pi running an instance of Magic Mirror
+* Another Raspberry Pi running an instance of OctoPi (or other OctoPrint instance)
+* IP addresses for both (static IP preferred)
 
 ## Installation
 
@@ -20,3 +28,28 @@ npm install
 # recommended to replace it with your server's version.
 # Replace 'http://octopi.local' with your OctoPrint's URL/IP
 wget http://octopi.local/static/webassets/packed_client.js -O packed_client.js
+````
+
+## Using the Module
+
+To use this module, add it to the modules array in the `config/config.js` file:
+
+```js
+    {
+        module: "octomirror-module",
+        position: "middle_center",
+        config: {
+            url: "http://octopi.local",
+            api_key: "[Octoprint API Key]"
+        }
+    },
+```
+
+### Configuration Options:
+
+| Option           | Description
+|----------------- |-----------
+| `url` | *Required* - The url or IP address for the OctoPrint Instance.
+| `api_key` | *Required* Your API Key from the OctoPrint service to be used.  You can find this in Octoprint's Options>Features>API, while you're there also enable Cross-Origin-Resorce-Sharing.
+| `interactive` | *Optional* Allow interactive control of the printer: choose files to print and upload new files. <br> *Default:* `true`. Set to `false` to hide the drop downs, if you don't use the Mirror to control anything.
+| `debugMode` | *Optional* Prints all messages received from the printer socket to the console log, for debugging only and developing more features.
--- octomirror-module.js
+++ octomirror-module.js
@@ -1,4 +1,5 @@
 /* global Module */
+/* jshint esversion: 6 */
 
 /* Magic Mirror
  * Module: OctoMirror-Module
@@ -11,8 +12,8 @@ Module.register("octomirror-module", {
     defaults: {
         updateInterval: 60 * 1000,
         retryDelay: 2500,
-        initialLoadDelay: 2500,
         interactive: true, // Set to false to hide the file drop down and only show the stream.
+        debugMode: false, // Set to true to log all messages from OctoPrint Socket
     },
 
     //Override dom generator.
@@ -51,7 +52,20 @@ Module.register("octomirror-module", {
             fileMenu.appendChild(fileUpload);
             wrapper.appendChild(fileMenu);
         }
+
+        var infoWrapper = document.createElement("div");
+        infoWrapper.className = "small";
+        infoWrapper.innerHTML = `<span>Printer State: </span><span id="opStateIcon"></span> <span id="opState" class="title bright"> </span>
+                <br />
+                <span>File: </span><span id="opFile" class="title bright">N/A</span>
+                <br />
+                <span> Time Elapsed: </span><span id="opPrintTime" class="title bright">N/A</span>
+                <span> | Time Remaining: </span><span id="opPrintTimeRemaining" class="title bright">N/A</span>
+                <span> | Percent Complete: </span><span id="opPercent" class="title bright">N/A</span>
+                `;
+
         wrapper.appendChild(stream);
+        wrapper.appendChild(infoWrapper);
         wrapper.appendChild(document.createElement("br"));
         return wrapper;
     },
@@ -66,18 +80,27 @@ Module.register("octomirror-module", {
         this.opClient = new OctoPrintClient();
         this.opClient.options.baseurl = this.config.url;
         this.opClient.options.apikey = this.config.api_key;
-
-        // Subscribe to live push updates from the server
-        this.opClient.socket.connect();
-        this.opClient.socket.onMessage("*", function(message) {
-            // TODO: Display progress of current print job
-            // Reference: http://docs.octoprint.org/en/master/api/push.html#sec-api-push-datamodel-currentandhistory
-            console.log("Octoprint", message);
-        });
     },
 
-    getHeader: function() {
-        return 'Octoprint!';
+    initializeSocket: function() {
+        var self = this;
+        // Subscribe to live push updates from the server
+        this.opClient.socket.connect();
+
+        if (this.config.debugMode) {
+            this.opClient.socket.onMessage("*", (message) => {
+                // Reference: http://docs.octoprint.org/en/master/api/push.html#sec-api-push-datamodel-currentandhistory
+                console.log("Octoprint", message);
+            });
+        }
+
+        this.opClient.socket.onMessage("history", (message) => {
+            this.updateData(message.data);
+        });
+
+        this.opClient.socket.onMessage("current", (message) => {
+            this.updateData(message.data);
+        });
     },
 
     getScripts: function() {
@@ -117,7 +140,6 @@ Module.register("octomirror-module", {
 
         this.opClient.files.list()
             .done(function(response) {
-                console.log(response);
                 self.processFiles(response);
             });
     },
@@ -132,5 +154,58 @@ Module.register("octomirror-module", {
             .done(function(response) {
                 self.updateFiles();
             });
+    },
+
+    updateData: function(data) {
+        document.getElementById("opState").textContent = (data.state.text.startsWith("Offline: SerialException")) ? "Offline / Disconnected" : data.state.text;
+        var icon = document.getElementById("opStateIcon");
+        if (data.state.flags.printing) {
+            icon.innerHTML = `<i class="fa fa-print" aria-hidden="true" style="color:green;"></i>`;
+        } else if (data.state.flags.closedOrError) {
+            icon.innerHTML = `<i class="fa fa-exclamation-triangle" aria-hidden="true" style="color:red;"></i>`;
+        } else if (data.state.flags.paused) {
+            icon.innerHTML = `<i class="fa fa-pause" aria-hidden="true" style="color:yellow;"></i>`;
+        } else if (data.state.flags.error) {
+            icon.innerHTML = `<i class="fa fa-exclamation-triangle" aria-hidden="true" style="color:red;"></i>`;
+        } else if (data.state.flags.ready) {
+            icon.innerHTML = `<i class="fa fa-check-circle" aria-hidden="true" style="color:green;"></i>`;
+        } else if (data.state.flags.operational) {
+            icon.innerHTML = `<i class="fa fa-check-circle" aria-hidden="true" style="color:green;"></i>`;
+        }
+        document.getElementById("opFile").textContent = (data.job.file.name) ? data.job.file.name : "N/A";
+        document.getElementById("opPrintTime").textContent = (data.progress.printTime) ? data.progress.printTime.toHHMMSS() : "N/A";
+        document.getElementById("opPrintTimeRemaining").textContent = (data.progress.printTimeLeft) ? data.progress.printTimeLeft.toHHMMSS() : "N/A";
+        document.getElementById("opPercent").textContent = (data.progress.completion) ? Math.round(data.progress.completion) + "%" : "N/A";
+    },
+
+    notificationReceived: function(notification, payload, sender) {
+        if (notification === 'DOM_OBJECTS_CREATED') {
+            this.initializeSocket();
+            this.scheduleUpdate(1);
+        }
     }
 });
+
+Number.prototype.toHHMMSS = function() {
+    var seconds = Math.floor(this),
+        hours = Math.floor(seconds / 3600);
+    seconds -= hours * 3600;
+    var minutes = Math.floor(seconds / 60);
+    seconds -= minutes * 60;
+
+    var time = "";
+
+    if (hours !== 0) {
+        time = hours + ":";
+    }
+    if (minutes !== 0 || time !== "") {
+        minutes = (minutes < 10 && time !== "") ? "0" + minutes : String(minutes);
+        time += minutes + ":";
+    }
+    if (time === "") {
+        time = seconds + "s";
+    } else {
+        time += (seconds < 10) ? "0" + seconds : String(seconds);
+    }
+    return time;
+};
